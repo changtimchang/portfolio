d6c1024cea1a278a9584a2168ab3f6139338ec4f
timchang <changtimchang@protonmail.com>
2024-10-19 17:08:26 +0900

    first

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,16 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter, Bebas_Neue } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const bebasNeue = Bebas_Neue({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-bebas",
 });
 
 export const metadata = {
@@ -20,11 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased ${inter.variable} ${bebasNeue.variable}`}>{children}</body>
     </html>
   );
 }
---
+++ components/About.jsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { motion } from "framer-motion";
+import AnimatedText from "./AnimatedText";
+
+const About = () => {
+  return (
+    <section className="relative pt-12 pb-24" id="about">
+      <div className="container mx-auto h-full">
+        <div className="h-full flex items-center justify-center">
+          {/* image & shapes */}
+          <div className="hidden xl:flex flex-1 pl-8">
+            <div className="relative w-full max-w-[380px]">
+              {/* shape */}
+              <div className="w-[160px] h-[160px] bg-accent absolute -left-5 -top-5 -z-10"></div>
+              {/* image */}
+              <div className="rounded-tl-[8px] rounded-tr-[120px] w-full bg-[#e5f8f6] min-h-[480px] flex items-end justify-center">
+                <Image
+                  src="/assets/about/img.png"
+                  width={350}
+                  height={478}
+                  quality={100}
+                  priority
+                  alt=""
+                />
+              </div>
+              {/* rotating shape */}
+              <div className="absolute top-2/4 -right-[65px] flex items-center justify-center">
+                <motion.div
+                  animate={{
+                    rotate: [0, 360],
+                  }}
+                  transition={{
+                    duration: 10,
+                    ease: "linear",
+                    repeat: Infinity,
+                  }}
+                >
+                  <Image
+                    src="/assets/about/shape-1.svg"
+                    width={160}
+                    height={160}
+                    alt=""
+                  />
+                </motion.div>
+                <div className="absolute text-center text-white">
+                  <div className="text-5xl font-bold leading-none"> 10+ </div>
+                  <div className="leading-none text-center">
+                    Years of <br /> Experience
+                  </div>
+                </div>
+              </div>
+            </div>
+          {/* text */}
+          <div className="text-center xl:text-left w-full xl:w-[50%] mx-auto xl:mx-0 flex flex-col gap-6">
+            <div>
+              <AnimatedText text="My name is Noah" textStyles="h2 mb-2" />
+              <p>Frontend Developer and Designer</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
